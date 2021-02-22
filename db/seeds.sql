a5f78ca4c3673f5307a8c62d4d4d2d8141a68fc9
MNM <moussanm2014@gmail.com>
2021-02-22 17:48:13 -0600

    modified db/seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Burger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", FALSE);
+INSERT INTO burgers (burger_name) VALUES ('Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
