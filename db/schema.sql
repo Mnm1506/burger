aa2ea97ea04b6b19ce1efe1d5db8971e25675fa6
MNM <moussanm2014@gmail.com>
2021-02-22 17:48:53 -0600

    modified db/schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+
+CREATE TABLE burgers 
+(
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
