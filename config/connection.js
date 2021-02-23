c0f20a51b3dbbc8bff9c2991cd4bd5d06bdabd66
MNM <moussanm2014@gmail.com>
2021-02-23 12:45:29 -0600

    modified connection.js file

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Akbou2009#",
-    database: "burgers_db"
+    password: "hacktheplanet",
+    database: "todoagain_db"
   });
 };
 // Make connection:
