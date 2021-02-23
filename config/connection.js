2e7046ab330f6f632775a876cd0c8300a87a49c6
MNM <moussanm2014@gmail.com>
2021-02-23 10:43:03 -0600

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -13,12 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 };
 // Make connection:
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
